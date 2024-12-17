eaf6c21eedefa57c41207a861db78e12e6d1d95b
Vidhi Patel <vidhi.patel@reversebits.tech>
2024-12-17 12:49:25 +0530

    Update variable file name

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/dart_language/language_module.dart';
+import 'package:flutter_demo/dart_language/variable_demo.dart';
 
 void main() {
   checkVariables();

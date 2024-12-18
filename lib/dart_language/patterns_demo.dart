f789e6cdca4fa97daa6614282ea271f1adfacc5e
Vidhi Patel <vidhi.patel@reversebits.tech>
2024-12-18 16:08:54 +0530

    Destructuring and Variable assignment pattern

---
+++ lib/dart_language/patterns_demo.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter_demo/dart_language/datatype_demo.dart';
+
+void checkPatterns() {
+
+  // Destructuring Pattern
+  var numList = [1, 2, 3];
+  var [a, b, c] = numList;
+  print("Sum = ${a+b+c}");
+
+  // Variable Assignment Pattern
+  var (x, y) = ("left", "right");
+  (y, x) = (x, y);
+  print("value of b: $y and value a: $x");
+}
